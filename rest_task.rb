0ea9071ce597a866fc91111547857163e87e94b7
SGospodinov <sani9706@gmail.com>
2016-01-09 22:01:01 +0200

    fixing input bugs

--- rest_task.rb
+++ rest_task.rb
@@ -1,13 +1,19 @@
 require 'net/http'
 require 'json'
 
+def numeric string
+    true if Float(string) != nil rescue false
+end
+
 id = ARGV[0].to_i
-if id > 0 and id <=100
+if id > 0 and id <=100 and numeric(ARGV[0])
 	url = "http://jsonplaceholder.typicode.com/posts/#{id}"
 	json = JSON.parse(Net::HTTP.get(URI(url)))
 	title = json["title"]
 	body = json["body"]
-	puts "Post title: #{title}\nPost body: #{body}"
+	userId = json["userId"]
+	postId = json["id"]
+	puts "Post title: #{title}\nPost body: #{body}\nPost id: #{postId}\nUser id: #{userId}"
 else
-	p "Wrong ID"
+	p "Wrong Input"
 end
